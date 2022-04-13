0d99f5e6b42d8d42aa18b50ea270a61140de86b1
AliciaRodrigues <rodriguesf.alicia@gmail.com>
2022-04-13 14:08:05 -0300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Alicia Rodrigues  on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
